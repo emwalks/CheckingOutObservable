a38ccc64be62abb253909fcc1def8f8d1015c3b0
Emma Walker <walkere@tcd.ie>
2023-12-17 13:23:51 +0000

    Initial Commit

---
+++ CheckingOutObservable/CheckingOutObservableApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckingOutObservableApp.swift
+//  CheckingOutObservable
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 17/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CheckingOutObservableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckingOutObservable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CheckingOutObservable
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 17/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
